f666a3b6283ef60ac410c01c9bcd111c376111c8
cTn <ctndesigner@gmail.com>
2013-07-11 19:24:15 +0200

    adding command log window to the UI

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function command_log(message) {
+    var d = new Date();
+    var time = ((d.getHours() < 10) ? '0' + d.getHours(): d.getHours()) 
+        + ':' + ((d.getMinutes() < 10) ? '0' + d.getMinutes(): d.getMinutes()) 
+        + ':' + ((d.getSeconds() < 10) ? '0' + d.getSeconds(): d.getSeconds());
+    
+    $('div#command-log > div.wrapper').append('<p>' + time + ' -- ' + message + '</p>');
+    $('div#command-log').scrollTop($('div#command-log div.wrapper').height());    
+}
