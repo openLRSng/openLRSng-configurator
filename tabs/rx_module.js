ed88700b0a56885a2194c808e28e94b8f02c7c90
cTn <ctndesigner@gmail.com>
2013-07-12 18:46:21 +0200

    beacon settings now available in RX tab

--- tabs/rx_module.js
+++ tabs/rx_module.js
@@ -11,6 +11,34 @@ function tab_initialize_rx_module() {
             $('input[name="failsafe_delay"]').val(RX_CONFIG.failsafe_delay);
             $('input[name="sync_time"]').val(RX_CONFIG.minsync);
             
+            if (RX_CONFIG.beacon_frequency == 0) { // disabled
+                $('select[name="beacon_frequency"]').val(0);
+            } else if (RX_CONFIG.beacon_frequency > 447000000) { // FRS
+                var calc = RX_CONFIG.beacon_frequency - 462537500;
+                
+                var chan = 0;
+                while (calc != 0) {
+                    calc -= 25000;
+                    chan++;
+                }
+                
+                $('select[name="beacon_frequency"]').val(chan);
+            } else { // PMR
+                var calc = RX_CONFIG.beacon_frequency - 445993750;
+                
+                var chan = 0;
+                while (calc != 0) {
+                    calc -= 12500;
+                    chan++;
+                }
+                
+                $('select[name="beacon_frequency"]').val(chan + 10); // + 10 because we are using the second sequence of channels
+            }
+            
+            
+            $('input[name="beacon_interval"]').val(RX_CONFIG.beacon_interval);
+            $('input[name="beacon_deadtime"]').val(RX_CONFIG.beacon_deadtime);
+            
             // UI Hooks
             $('a.restore').click(function() {
                 send_message(PSP.PSP_SET_RX_RESTORE_DEFAULT, 1);
@@ -29,6 +57,22 @@ function tab_initialize_rx_module() {
                 // send this object to the module and then request EEPROM save
                 RX_CONFIG.failsafe_delay = parseInt($('input[name="failsafe_delay"]').val());
                 RX_CONFIG.minsync = parseInt($('input[name="sync_time"]').val());
+                
+                var temp_beacon_frequency = parseInt($('select[name="beacon_frequency"]').val());
+                if (temp_beacon_frequency == 0) {
+                    RX_CONFIG.beacon_frequency = 0;
+                } else if (temp_beacon_frequency < 8) { // FRS
+                    var calc = 462537500 + 25000 * temp_beacon_frequency;
+                    
+                    RX_CONFIG.beacon_frequency = calc;
+                } else { // PMR
+                    var calc = 445993750 + 12500 * (temp_beacon_frequency - 10); // - 10 because we are using the second sequence of channels
+                    
+                    RX_CONFIG.beacon_frequency = calc;
+                }
+                
+                RX_CONFIG.beacon_interval = parseInt($('input[name="beacon_interval"]').val());
+                RX_CONFIG.beacon_deadtime = parseInt($('input[name="beacon_deadtime"]').val());
                 
                 send_RX_config();
             });
