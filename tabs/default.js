7f7bd5718cf015fbea3ea5bbe8971e5cc9fdfb9a
cTn <ctndesigner@gmail.com>
2013-08-05 00:07:06 +0200

    added firmware flasher button

--- tabs/default.js
+++ tabs/default.js
@@ -1,6 +1,6 @@
 function tab_initialize_default() {
     $('#content').load("./tabs/default.html", function() {
-        $('.tab-default a.firmware_upload').click(function() {
+        $('.tab-default a.firmware_upload, .tab-default a.firmware_upload_button').click(function() {
             tab_initialize_uploader();
         });
     
