33caf2eb0ebb3752722c94b1c583d638bd1459a9
cTn <ctndesigner@gmail.com>
2013-08-19 13:47:24 +0200

    customizable hop channel order & frequencies

--- tabs/tx_module.js
+++ tabs/tx_module.js
@@ -132,7 +132,10 @@ function tab_initialize_tx_module() {
                 }
             }
             
-            if (validation_result) {
+            // fire change event on hop_channel list elemets to run custom_hop_list validation
+            $('div.hop_channels ul.list input:first').change();
+            
+            if (validation_result && custom_hopchannel_list_valid) {
                 // Basic settings
                 // we need to "grasp" all values from the UI, store it in the local BIND_DATA object
                 // send this object to the module and then request EEPROM save
@@ -237,19 +240,21 @@ function generate_info_list() {
     } else {
         hopcount = 1;
     }
+    
     for (var i = 0; i < hopcount; i++) {
-        var out = (base_fequency + BIND_DATA.hopchannel[i] * channel_spacing * 10000) / 1000; // kHz
+        var output = (base_fequency + BIND_DATA.hopchannel[i] * channel_spacing * 10000) / 1000; // kHz
         
         if (BIND_DATA.hopchannel[i] != 0) {
-            //$('div.hop_channels ul.list').eq(list).append("<li> Hop <input name=\"hopchan\" type=\"number\" min=\"1\" max=\"" + hopcount + "\" value=\"" + (i + 1) + "\"/> - " + out + " kHz</li>");
-            $('div.hop_channels ul.list').eq(list).append("<li> Hop " + (i + 1) + " - " + out + " kHz</li>");
+            $('div.hop_channels ul.list').eq(list).append('<li><input class="hopchan" name="hopchan" type="number" min="1" max="' + hopcount + '" value="' + (i + 1) + '"/></li>');
+            $('div.hop_channels ul.list li').last().append('<input class="chan_value" name="chan_value" type="number" step="' + (channel_spacing * 10) + '" value="' + output + '"/> kHz');
+            //$('div.hop_channels ul.list').eq(list).append("<li> Hop " + (i + 1) + " - " + output + " kHz</li>");
             
             // save hopchannel index in data for later comparison
             //$('div.hop_channels ul.list').eq(list).find('input').last().data('oldVal', (i + 1));
         } else {
             // we dropped here because hopchannel for this hop couldn't be generated (desired frequency range is too small)
             // all of the failed chanells will be visually marked as red
-            $('div.hop_channels ul.list').eq(list).append("<li style=\"color: red;\"> Hop " + (i + 1) + " - " + out + " kHz</li>");
+            $('div.hop_channels ul.list').eq(list).append('<li style="color: red"> Hop ' + (i + 1) + ' - ' + output + ' kHz</li>');
         }
         
         // switch lists if necessary
@@ -258,31 +263,128 @@ function generate_info_list() {
         }
         
         // check the frequency
-        if (max_frequency < out) {
-            max_frequency = out;
+        if (max_frequency < output) {
+            max_frequency = output;
         }
     }
     
     // Update Max Frequency
     $('.maximum_frequency').html(max_frequency + ' kHz');
     
-    /*
     // bind UI hooks for newly generated list
     $('div.hop_channels ul.list input').change(function() {
-        var old_index = $(this).data('oldVal') - 1;
-        var new_index = parseInt($(this).val()) - 1;
+        custom_hopchannel_list_valid = false;
         
-        var old_value = BIND_DATA.hopchannel[old_index];
-        var new_value = BIND_DATA.hopchannel[new_index];
+        // 1. bound validation
+        var bound_validation = true;
+        $('div.hop_channels ul.list input.hopchan').each(function() {
+            if (!validate_input_bounds($(this))) {
+                bound_validation = false;
+            }
+        });
         
-        // swap
-        BIND_DATA.hopchannel[old_index] = new_value;
-        BIND_DATA.hopchannel[new_index] = old_value;
+        // 2. index validation
+        if (bound_validation) {
+            var index_validation = true;
+            
+            var temp_array = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0); // blank 24 field array
+            
+            $('div.hop_channels ul.list input.hopchan').each(function() {
+                var index = parseInt($(this).val());
+                if (temp_array.indexOf(index) == -1) {
+                    // index is not yet in the array, save it
+                    temp_array[index] = index;
+                    
+                    $(this).removeClass('validation_failed');
+                } else {
+                    // index is already in array, failed
+                    index_validation = false;
+                    
+                    $(this).addClass('validation_failed');
+                }
+            });
+        }
         
-        // re-generate info list
-        generate_info_list();
+        // 3. chanvalue validation
+        if (index_validation) {
+            var chanvalue_validation = true;
+            
+            // generate helper array
+            var base_fequency = parseInt($('input[name="operating_frequency"]').val() * 1000);
+            var channel_spacing = parseInt($('input[name="channel_spacing"]').val());
+            var maximum_desired_frequency = parseInt($('input[name="maximum_desired_frequency"]').val() * 1000);
+            var valid_freq = new Array();
+            var new_hopchannel = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0); // blank 24 field array
+            
+            for (var i = 0; i < 256; i++) { // starting at first channel
+                var output = (base_fequency + i * channel_spacing * 10000) / 1000; // kHz
+                
+                if (output > (maximum_desired_frequency / 1000)) {
+                    // break on hitting the maximum frequency desired by the user
+                    break;
+                }
+                
+                valid_freq.push(output);
+            }
+            
+            $('div.hop_channels ul.list input.chan_value').each(function() {
+                var val = parseInt($(this).val());
+                var index = parseInt($(this).parent().find('input.hopchan').val()) - 1; // input channels start with 1, but array starts with 0
+                
+                var match_found = false;
+                for (var i = 0; i < valid_freq.length; i++) {
+                    if (valid_freq[i] == val) {
+                        match_found = true;
+                        
+                        new_hopchannel[index] = i;
+                        
+                        $(this).removeClass('validation_failed');
+                        break;
+                    }
+                }
+                
+                if (!match_found) {
+                    chanvalue_validation = false;
+                    
+                    $(this).addClass('validation_failed');
+                }
+            });
+        }
+        
+        // 4. value duplicity validation
+        if (chanvalue_validation) {
+            var channel_duplicity_validation = true;
+            
+            var temp_array = new Array();
+            
+            $('div.hop_channels ul.list input.chan_value').each(function() {
+                var val = parseInt($(this).val());
+
+                for (var i = 0; i < temp_array.length; i++) {
+                    if (temp_array[i] == val) {
+                        // match found, failed
+                        channel_duplicity_validation = false;
+                        
+                        $(this).addClass('validation_failed');
+                        
+                        break;
+                    }
+                }
+                
+                if (channel_duplicity_validation) {
+                    temp_array.push(val);
+                    $(this).removeClass('validation_failed');
+                }
+            });
+        }
+        
+        // all is good, replace arrays
+        if (channel_duplicity_validation) {
+            BIND_DATA.hopchannel = new_hopchannel;
+            
+            custom_hopchannel_list_valid = true;
+        }
     });
-    */
 }
 
 function generate_info_refresh() {
