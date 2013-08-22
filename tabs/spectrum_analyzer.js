98de2252bf3f3c6740a4ef37a950f37cec4ee5d0
cTn <ctndesigner@gmail.com>
2013-08-22 19:19:39 +0200

    initial SA code for pause/resume functionality

--- tabs/spectrum_analyzer.js
+++ tabs/spectrum_analyzer.js
@@ -110,8 +110,45 @@ function tab_initialize_spectrum_analyzer() {
         // manually fire change event so variables get populated
         $('div#analyzer-configuration select').change(); 
         
-        // Pause handler is missing here !!!
-        
+        // Pause/Resume handler
+        $('.pause-resume').click(function() {
+            console.log('Pause/Resume functionality is not yet implemented');
+            command_log('Pause/Resume functionality is not yet implemented');
+            /*
+            var clicks = $(this).data('clicks');
+            
+            if (clicks) { // odd number of clicks
+                if (connectionId != -1) {
+                    // empty buffer manually (.flush doesn't seem to work here for some reason)
+                    chrome.serial.read(connectionId, 1048575, function() {});
+                    
+                    serial_poll = setInterval(readPoll, 10);
+                    plot_poll = setInterval(redraw_plot, 40);
+                    
+                    plot_options.mouse.track = false;
+                }
+                
+                $(this).text('Pause');
+                $(this).removeClass('resume');            
+            } else { // even number of clicks
+                try {
+                    clearInterval(serial_poll);
+                    clearInterval(plot_poll);
+                } catch (error) {
+                    //if one of the interval references doesn't exist, we will "stop" / return
+                    return;
+                };
+                
+                plot_options.mouse.track = true;
+                redraw_plot();
+                
+                $(this).text('Resume');  
+                $(this).addClass('resume');
+            }
+            
+            $(this).data("clicks", !clicks);      
+            */
+        });        
         
         // Plot
         element_plot = document.getElementById("plot");
