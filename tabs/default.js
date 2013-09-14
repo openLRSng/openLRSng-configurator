f30aafbd68bfc913e98699ad91718b71f4a8d5f7
cTn <ctndesigner@gmail.com>
2013-09-14 14:33:35 +0200

    moving changelog content to separate file

--- tabs/default.js
+++ tabs/default.js
@@ -1,5 +1,9 @@
 function tab_initialize_default() {
     $('#content').load("./tabs/default.html", function() {
+        // load changelog content
+        $('div.changelog .wrapper').load('./changelog.html');
+        
+        // UI hooks
         $('.tab-default a.firmware_upload, .tab-default a.firmware_upload_button').click(function() {
             tab_initialize_uploader();
         });
