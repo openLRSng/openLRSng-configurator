66fbc3e4b706f0a5b8d31adfd2e92fd6c9bc190f
cTn <ctndesigner@gmail.com>
2013-11-13 09:59:05 +0100

    routine update

--- js/protocols/stm32.js
+++ js/protocols/stm32.js
@@ -109,6 +109,7 @@ STM32_protocol.prototype.initialize = function() {
     }, 1000);
     
     // there seems to be 2 unwanted bytes in the parsed array, we will drop them now (WHY ???)
+    // TODO: This needs to be reworked ASAP !!!
     this.hex_to_flash.shift();
     this.hex_to_flash.shift();
     
