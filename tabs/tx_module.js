58a7ee24cc39d5aa62c8a3e335544787a3ce80cd
cTn <ctndesigner@gmail.com>
2013-11-19 16:08:17 +0100

    cleanup

--- tabs/tx_module.js
+++ tabs/tx_module.js
@@ -434,8 +434,7 @@ function generate_info() {
     
     var ms = ((packet_sizes[parseInt($('select[name="channel_config"]').val()) - 1] + 15) * 8200000) / data_rates[parseInt($('select[name="data_rate"]').val())] + 2000;
     
-    var telemetry = parseInt($('select[name="telemetry"]').val());
-    if (telemetry >= 1) {
+    if (parseInt($('select[name="telemetry"]').val()) >= 1) {
         ms += (((9 + 15) * 8200000) / data_rates[parseInt($('select[name="data_rate"]').val())]) + 1000;
     }
     
