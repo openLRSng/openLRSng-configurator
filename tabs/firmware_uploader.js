7798eec40f1f042fa5486784806bc0c3be796969
cTn <ctndesigner@gmail.com>
2013-12-06 22:41:19 +0100

    implementing custom firmware upload
    #3

--- tabs/firmware_uploader.js
+++ tabs/firmware_uploader.js
@@ -17,6 +17,10 @@ function tab_initialize_uploader() {
                 // "callback"
                 worker.onmessage = function (event) {
                     uploader_hex_parsed = event.data;
+                    
+                    $('div.firmware_info .type').html('Embedded Firmware');
+                    $('div.firmware_info .version').html(firmware_version_accepted[0] + '.' + firmware_version_accepted[1] + '.' + firmware_version_accepted[2]);
+                    $('div.firmware_info .size').html(uploader_hex_parsed.bytes + ' bytes');
                 };
                 
                 // send data/string over for processing
@@ -24,24 +28,92 @@ function tab_initialize_uploader() {
             });
         });
         
+        $('a.load_custom_firmware').click(function() {
+            chrome.fileSystem.chooseEntry({type: 'openFile', accepts: [{extensions: ['hex']}]}, function(fileEntry) {
+                if (!fileEntry) {
+                    // no "valid" file selected/created, aborting
+                    console.log('No valid file selected, aborting');
+                    return;
+                }
+                
+                chrome.fileSystem.getDisplayPath(fileEntry, function(path) {
+                    console.log('Loading file from: ' + path);
+                    
+                    fileEntry.file(function(file) {
+                        var reader = new FileReader();
+
+                        reader.onerror = function (e) {
+                            console.error(e);
+                        };
+                        
+                        reader.onloadend = function(e) {
+                            console.log('File loaded');
+                            
+                            // parsing hex in different thread
+                            var worker = new Worker('./workers/hex_parser.js');
+                            
+                            // "callback"
+                            worker.onmessage = function (event) {
+                                uploader_hex_parsed = event.data;
+                                
+                                if (uploader_hex_parsed) {
+                                    $('div.firmware_info .type').html('Custom Firmware');
+                                    $('div.firmware_info .version').html('Unknown');
+                                    $('div.firmware_info .size').html(uploader_hex_parsed.bytes + ' bytes');
+                                } else {
+                                    $('div.firmware_info .type').html('Firmware Corrupted');
+                                    $('div.firmware_info .version').html('Unknown');
+                                    $('div.firmware_info .size').html('Firmware Corrupted');
+                                
+                                    command_log('HEX file appears to be <span style="color: red">corrupted</span>');
+                                }
+                            };
+                            
+                            // send data/string over for processing
+                            worker.postMessage(e.target.result);
+                        };
+
+                        reader.readAsText(file);
+                    });
+                });
+            });
+        });
+        
         $('a.flash').click(function() {
             if (!GUI.connect_lock) { // button disabled while flashing is in progress
-                if ($('input[name="selected_firmware"]').is(':checked') && uploader_hex_parsed) { // only allow flashing if firmware was selected and hexfile is valid
-                    if ($('input[name="selected_firmware"]:checked').val() == 'TX-6') {
-                        // AVR109 protocol based arduino bootloaders
-                        AVR109.hex = uploader_hex_parsed;
-                        AVR109.connect();
-                    } else if ($('input[name="selected_firmware"]:checked').val() == 'RX-32') {
-                        // STM32 protocol based bootloaders
-                        STM32.hex = uploader_hex_parsed;
-                        STM32.connect();
+                // only allow flashing if firmware was selected and hexfile is valid
+                if (uploader_hex_parsed) {
+                    if ($('input[name="selected_firmware"]').is(':checked')) {
+                        if ($('input[name="selected_firmware"]:checked').val() == 'TX-6') {
+                            // AVR109 protocol based arduino bootloaders
+                            if (uploader_hex_parsed.bytes <= 28672) { // don't allow to go over-allowed flash (might be better to implement this inside flash protocol)
+                                AVR109.hex = uploader_hex_parsed;
+                                AVR109.connect();
+                            } else {
+                                command_log('Firmware size is <span style="color: red">too big</span>, did you loaded the correct firmware for selected board?');
+                            }
+                        } else if ($('input[name="selected_firmware"]:checked').val() == 'RX-32') {
+                            // STM32 protocol based bootloaders
+                            if (uploader_hex_parsed.bytes <= 131072) { // don't allow to go over-allowed flash (might be better to implement this inside flash protocol)
+                                STM32.hex = uploader_hex_parsed;
+                                STM32.connect();
+                            } else {
+                                command_log('Firmware size is <span style="color: red">too big</span>, did you loaded the correct firmware for selected board?');
+                            }
+                        } else {
+                            // STK500 protocol based arduino bootloaders
+                            if (uploader_hex_parsed.bytes <= 30720) { // don't allow to go over-allowed flash (might be better to implement this inside flash protocol)
+                                STK500.hex = uploader_hex_parsed;
+                                STK500.connect();
+                            } else {
+                                command_log('Firmware size is <span style="color: red">too big</span>, did you loaded the correct firmware for selected board?');
+                            }
+                        }
                     } else {
-                        // STK500 protocol based arduino bootloaders
-                        STK500.hex = uploader_hex_parsed;
-                        STK500.connect();
+                        command_log('Please first <strong>Select Board</strong> from the menu below');
                     }
                 } else {
-                    command_log('Please first select firmware from the menu below');
+                    command_log('Can not flash <span style="color: red">corrupted</span> firmware, please select different HEX file or re-select board to load embedded firmware');
                 }
             }
         });
