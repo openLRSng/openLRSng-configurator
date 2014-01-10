a09bd3e409453ef0677069d6b811bd6a01a6df13
cTn <ctndesigner@gmail.com>
2014-01-10 23:09:26 +0100

    optimize

--- js/gui.js
+++ js/gui.js
@@ -244,13 +244,14 @@ GUI_control.prototype.timeout_kill_all = function() {
 
 // message = string
 GUI_control.prototype.log = function(message) {
+    var command_log = $('div#command-log');
     var d = new Date();
     var time = ((d.getHours() < 10) ? '0' + d.getHours(): d.getHours()) 
         + ':' + ((d.getMinutes() < 10) ? '0' + d.getMinutes(): d.getMinutes()) 
         + ':' + ((d.getSeconds() < 10) ? '0' + d.getSeconds(): d.getSeconds());
     
-    $('div#command-log > div.wrapper').append('<p>' + time + ' -- ' + message + '</p>');
-    $('div#command-log').scrollTop($('div#command-log div.wrapper').height());   
+    $('div.wrapper', command_log).append('<p>' + time + ' -- ' + message + '</p>');
+    command_log.scrollTop($('div.wrapper', command_log).height());   
 };
 
 // Method is called every time a valid tab change event is received
