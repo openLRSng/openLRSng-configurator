1f0020f21f52a9dad3c50dedaf464ad1f0d40cb5
cTn <ctndesigner@gmail.com>
2014-01-17 12:51:02 +0100

    extending support

--- js/serial.js
+++ js/serial.js
@@ -1,9 +1,16 @@
 var serial = {
     connectionId: -1,
     
-    connect: function() {
+    connect: function(path, options, callback) {
+        var self = this;
+        
+        chrome.serial.connect(path, options, function(connectionInfo) {
+            self.connectionId = connectionInfo.connectionId;
+            callback(connectionInfo);
+        });
     },
-    disconnect: function() {
+    disconnect: function(callback) {
+        chrome.serial.disconnect(this.connectionId, callback);
     },
     getDevices: function(callback) {
         chrome.serial.getDevices(function(devices_array) {
@@ -14,5 +21,19 @@ var serial = {
             
             callback(devices);
         });
+    },
+    send: function(data, callback) {
+        chrome.serial.send(this.connectionId, data, callback);
+    },
+    onReceive: {
+        listeners_: chrome.serial.onReceive.listeners_,
+        
+        addListener: function(function_reference) {
+            chrome.serial.onReceive.addListener(function_reference);
+            //this.listeners_.push(function_reference);
+        },
+        removeListener: function(function_reference) {
+            chrome.serial.onReceive.removeListener(function_reference);
+        }
     }
 };
