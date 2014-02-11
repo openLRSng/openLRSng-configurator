e357101e81093ffd555187c0f7b9cbf4e8ba2140
cTn <ctndesigner@gmail.com>
2014-02-11 10:59:06 +0100

    cache messages (for offline processing)

--- js/developer_notify.js
+++ js/developer_notify.js
@@ -19,23 +19,48 @@ function request_developer_notify() {
                 return;
             }
             
-            obj.messages.forEach(function(message) {
-                if (message.chromium_main_version == parseInt(chromium_version.split('.')[0])) {
+            // cache messages
+            chrome.storage.local.get('developer_notify_cache', function(result) {
+                if (typeof result.developer_notify_cache !== 'undefined') {
+                    // if cache is old or time == false, save
+                    if (result.developer_notify_cache.time < obj.time || obj.time == false) {
+                        chrome.storage.local.set({'developer_notify_cache': obj}, function() {});
+                    }
+                } else {
+                    // cache wasn't saved yet, save
+                    chrome.storage.local.set({'developer_notify_cache': obj}, function() {});
+                }
+            });
+            
+            process_messages(obj);
+            
+        })
+        .fail(function() {
+            // use cached message (if available)
+            chrome.storage.local.get('developer_notify_cache', function(result) {
+                if (typeof result.developer_notify_cache !== 'undefined') {
+                    process_messages(result.developer_notify_cache);
+                }
+            });
+        });
+        
+    var process_messages = function(obj) {
+        obj.messages.forEach(function(message) {
+            if (message.chromium_main_version == parseInt(chromium_version.split('.')[0])) {
+                if (message.platform == GUI.operating_system || message.platform == false) {
+                    message.text.forEach(function(text) {
+                        GUI.log(text);
+                    });
+                }
+            } else if (message.chromium_main_version == false) {
+                if (message.chromium_version == chromium_version || message.chromium_version == false) {
                     if (message.platform == GUI.operating_system || message.platform == false) {
                         message.text.forEach(function(text) {
                             GUI.log(text);
                         });
                     }
-                } else if (message.chromium_main_version == false) {
-                    if (message.chromium_version == chromium_version || message.chromium_version == false) {
-                        if (message.platform == GUI.operating_system || message.platform == false) {
-                            message.text.forEach(function(text) {
-                                GUI.log(text);
-                            });
-                        }
-                    }
                 }
-            });
-            
+            }
         });
+    };
 }
