326b682980977b9d64d7edce2b1dfc59686f3d07
cTn <ctndesigner@gmail.com>
2014-04-12 19:02:50 +0200

    dropping in latest D3

--- js/gui.js
+++ js/gui.js
@@ -32,6 +32,7 @@ function GUI_control() {
     else if (navigator.appVersion.indexOf("CrOS") != -1)    this.operating_system = "ChromeOS";
     else if (navigator.appVersion.indexOf("Linux") != -1)   this.operating_system = "Linux";
     else if (navigator.appVersion.indexOf("X11") != -1)     this.operating_system = "UNIX";
+    else this.operating_system = "Unknown";
 }
 
 // Tab managing methods
