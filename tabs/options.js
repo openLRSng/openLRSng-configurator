bfc4d866f3312323146b5bad71414e690746d6ba
cTn <ctndesigner@gmail.com>
2014-04-21 23:40:08 +0200

    desktop notifications OPT out

--- tabs/options.js
+++ tabs/options.js
@@ -16,6 +16,17 @@ function tab_initialize_options(status) {
             $('a.back').hide();
         }
 
+        // if RTS is enabled, check the rts checkbox
+        if (GUI.disable_quickjoin == true) {
+            $('div.quickjoin input').prop('checked', true);
+        }
+
+        $('div.quickjoin input').change(function() {
+            GUI.disable_quickjoin = $(this).is(':checked');
+
+            chrome.storage.local.set({'disable_quickjoin': GUI.disable_quickjoin});
+        });
+
         // if tracking is enabled, check the statistics checkbox
         if (ga_tracking == true) {
             $('div.statistics input').prop('checked', true);
@@ -29,15 +40,17 @@ function tab_initialize_options(status) {
             ga_config.setTrackingPermitted(check);
         });
 
-        // if RTS is enabled, check the rts checkbox
-        if (GUI.disable_quickjoin == true) {
-            $('div.quickjoin input').prop('checked', true);
-        }
+        // if notifications are enabled, or wasn't set, check the notifications checkbox
+        chrome.storage.local.get('update_notify', function(result) {
+            if (typeof result.update_notify === 'undefined' || result.update_notify) {
+                $('div.notifications input').prop('checked', true);
+            }
+        });
 
-        $('div.quickjoin input').change(function() {
-            GUI.disable_quickjoin = $(this).is(':checked');
+        $('div.notifications input').change(function() {
+            var check = $(this).is(':checked');
 
-            chrome.storage.local.set({'disable_quickjoin': GUI.disable_quickjoin});
+            chrome.storage.local.set({'update_notify': check});
         });
     });
 }
