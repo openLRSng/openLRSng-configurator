9d479487308dc629cc3d5ea8bb64bcea7f0d48f5
cTn <ctndesigner@gmail.com>
2014-05-31 13:51:51 +0200

    initial implementation of channel mapping UI

--- tabs/signal_monitor.js
+++ tabs/signal_monitor.js
@@ -11,23 +11,46 @@ function tab_initialize_signal_monitor() {
 
         var status = $('.tab-signal_monitor .status .indicator');
         var bars = $('.tab-signal_monitor .bars');
-        for (var i = 0; i < PPM.channels.length; i++) {
-            bars.append('\
-                <div class="bar">\
-                    <div class="name">Channel - ' + (i + 1) + '</div>\
-                    <meter min="800" max="2200" low="1000" high="2000"></meter>\
-                    <div class="value"></div>\
-                </div>\
-            ');
+
+        // prepare generic options
+        var options = "";
+        for (var i = 0, analog = 0; i < 18; i++) {
+            if (i < 16) {
+                options += '<option value="' + i +'">' + chrome.i18n.getMessage('signal_monitor_channel', [i + 1]) + '</option>';
+            } else {
+                options += '<option value="' + i + '">' + chrome.i18n.getMessage('signal_monitor_analog', [analog++]) + '</option>';
+            }
         }
 
+        // spawn each line
+        for (var i = 0; i < PPM.channels.length; i++) {
+            bars.append('\
+                <tr class="bar">\
+                    <td class="source">' + chrome.i18n.getMessage('signal_monitor_channel', [i + 1]) + '</td>\
+                    <td class="output"><select>' + options + '</select></td>\
+                    <td class="meter"><meter min="800" max="2200" low="1000" high="2000"></meter></td>\
+                    <td class="value"></td>\
+                </tr>\
+            ');
+            bars.find('tr:last .output select').val(TX_CONFIG.chmap[i]);
+        }
+
+        $('a.save_to_eeprom').click(function() {
+            var i = 0;
+            $('.output select', bars).each(function() {
+                TX_CONFIG.chmap[i++] = parseInt($(this).val());
+            });
+
+            send_TX_config();
+        });
+
         var meter_array = [];
-        $('meter', bars).each(function() {
+        $('td.meter meter', bars).each(function() {
             meter_array.push($(this));
         });
 
         var meter_values_array = [];
-        $('.value', bars).each(function() {
+        $('td.value', bars).each(function() {
             meter_values_array.push($(this));
         });
 
