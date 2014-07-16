36362a51caa78b793ee19e00552ccf64427882ea
cTn <ctndesigner@gmail.com>
2014-07-16 13:54:24 +0200

    use exceptions instead of events for error reporting

--- js/serial.js
+++ js/serial.js
@@ -25,7 +25,7 @@ var serial = {
 
                 self.onReceiveError.addListener(function watch_for_on_receive_errors(info) {
                     console.error(info);
-                    googleAnalytics.sendEvent('Error', 'Serial', info.error);
+                    googleAnalytics.sendException('Serial: ' + info.error, false);
 
                     switch (info.error) {
                         case 'system_error': // we might be able to recover from this one
@@ -38,11 +38,11 @@ var serial = {
                             function crunch_status(info) {
                                 if (!info.paused) {
                                     console.log('SERIAL: Connection recovered from last onReceiveError');
-                                    googleAnalytics.sendEvent('Error', 'Serial', 'recovered');
+                                    googleAnalytics.sendException('Serial: onReceiveError - recovered', false);
                                 } else {
                                     console.log('SERIAL: Connection did not recover from last onReceiveError, disconnecting');
                                     GUI.log('Unrecoverable <span style="color: red">failure</span> of serial connection, disconnecting...');
-                                    googleAnalytics.sendEvent('Error', 'Serial', 'unrecoverable');
+                                    googleAnalytics.sendException('Serial: onReceiveError - unrecoverable', false);
 
                                     if (GUI.connected_to || GUI.connecting_to) {
                                         $('a.connect').click();
@@ -98,7 +98,7 @@ var serial = {
                 pre_up();
             } else {
                 console.log('SERIAL: Failed to open serial port');
-                googleAnalytics.sendEvent('Error', 'Serial', 'FailedToOpen');
+                googleAnalytics.sendException('Serial: FailedToOpen', false);
 
                 if (callback) callback(false);
             }
@@ -131,7 +131,7 @@ var serial = {
                     console.log('SERIAL: Connection with ID: ' + self.connectionId + ' closed');
                 } else {
                     console.log('SERIAL: Failed to close connection with ID: ' + self.connectionId + ' closed');
-                    googleAnalytics.sendEvent('Error', 'Serial', 'FailedToClose');
+                    googleAnalytics.sendException('Serial: FailedToClose', false);
                 }
 
                 console.log('SERIAL: Statistics - Sent: ' + self.bytes_sent + ' bytes, Received: ' + self.bytes_received + ' bytes');
