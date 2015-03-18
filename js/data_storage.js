595aa5b4e9920c8faae1648e85c68339b409c440
cTn <ctndesigner@gmail.com>
2015-03-18 18:59:28 +0100

    update firmware to 3.8.1, update changelog, update data storage to accept 3.8.1

--- js/data_storage.js
+++ js/data_storage.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var CONFIGURATOR = {
-    'releaseDate':              1423425814787, // 02.08.2015 - new Date().getTime()
-    'firmwareVersionEmbedded':  [3, 8, 0], // version of firmware that ships with the app, dont forget to also update initialize_configuration_objects switch !
+    'releaseDate':              1426701128661, // 2015.03.18 - new Date().getTime()
+    'firmwareVersionEmbedded':  [3, 8, 1], // version of firmware that ships with the app, dont forget to also update initialize_configuration_objects switch !
     'firmwareVersionLive':      0, // version number in single uint16 [8bit major][4bit][4bit] fetched from mcu
     'activeProfile':            0, // currently active profile on tx module (each profile can correspond to different BIND_DATA)
     'defaultProfile':           0, // current default profile setting on tx module
@@ -82,6 +82,7 @@ function initializeFrequencyLimits(rfmType) {
 
 function initialize_configuration_objects(version) {
     switch (version) {
+        case 0x381:
         case 0x380:
         case 0x374:
         case 0x373:
