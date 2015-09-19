01753245b9fa8e50d6469a343f0692d94f5224a5
Stefan Kolla <ctndesigner@gmail.com>
2015-09-19 15:22:42 +0200

    bump release date and manifest

--- js/data_storage.js
+++ js/data_storage.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var CONFIGURATOR = {
-    'releaseDate':              1433099837000, // 2015.05.31 - new Date().getTime()
+    'releaseDate':              1442668874049, // 2015.09.19 - new Date().getTime()
     'firmwareVersionEmbedded':  [3, 8, 5], // version of firmware that ships with the app, dont forget to also update initialize_configuration_objects switch !
     'firmwareVersionLive':      0, // version number in single uint16 [8bit major][4bit][4bit] fetched from mcu
     'activeProfile':            0, // currently active profile on tx module (each profile can correspond to different BIND_DATA)
